03efa6095bbc8f83fed901f9de1a3774792ff944
lopestorm <lopestom1@gmail.com>
2022-02-17 15:41:43 -0300

    Update twrp_S88Pro.mk

--- twrp_S88Pro.mk
+++ twrp_S88Pro.mk
@@ -10,7 +10,7 @@ $(call inherit-product, device/doogee/S88Pro/device.mk)
 
 # Inherit some common Omni stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
-$(call inherit-product, vendor/twrp/config/gsm.mk)
+#$(call inherit-product, vendor/twrp/config/gsm.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := S88Pro
