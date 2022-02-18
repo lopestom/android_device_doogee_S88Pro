348fed53c62a7ba5880fb86c76eaaa5c0c164bfe
lopestorm <lopestom1@gmail.com>
2022-02-18 10:40:08 -0300

    Update twrp_S88Pro.mk

--- twrp_S88Pro.mk
+++ twrp_S88Pro.mk
@@ -2,8 +2,9 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+#$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+#$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 
 # Inherit from X30 device
 $(call inherit-product, device/doogee/S88Pro/device.mk)
