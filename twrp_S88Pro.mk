d9b443a06110d4d8221ca20238cb8c0d86ac3ba7
lopestorm <guillopes2002@gmail.com>
2022-07-27 16:51:14 -0300

    Update twrp_S88Pro.mk

--- twrp_S88Pro.mk
+++ twrp_S88Pro.mk
@@ -2,8 +2,7 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-#$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-#$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+#$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
 # Inherit from X30 device
